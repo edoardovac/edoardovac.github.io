af5d531f4dac1740426083545fea33ee382ffef6
edoardovac <113552591+edoardovac@users.noreply.github.com>
2022-11-29 15:25:32 +0200

    Create image_list.js

---
+++ digi-intro-project/image_list.js
@@ -0,0 +1,40 @@
+function showParrot() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="parrot.jpg" alt="yellow ara"/>';
+}
+function showPumpikin() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="pumpkin.jpg" alt="various pumpkins with a basket"/>';
+}
+function showSkyline() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="buildings.jpg" alt="skyline of a city at dawn"/>';
+}
+function showMannequin() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="fashion.jpg" alt="two mannequins"/>';
+}
+function showElephant() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="elephant.jpg" alt="an elephant"/>';
+}
+function showMediterranean() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="olive-oil.jpg" alt=still life with oil tomatoes and basil" />';
+}
+function showPark() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="park.jpg" alt="a red park"/>';
+}
+function showTurtle() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="turtle.jpg" alt="swimming turtle"/>';
+}
+function showFruits() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="still-life.jpg" alt="various fruits with a jug in the middle"/>';
+}
+function showBridge() {
+  document.getElementById("img-container").innerHTML =
+    '<img src="bridge.jpg" alt="bridge"/>';
+}
